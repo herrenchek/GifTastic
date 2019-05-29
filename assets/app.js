be25c87f4badb13535589b4fb75947689e5591bd
Giselle Malenchek <Giselle@Giselles-MBP.fios-router.home>
2019-05-28 22:58:56 -0400

    Query URL implemented

--- assets/app.js
+++ assets/app.js
@@ -3,19 +3,35 @@ $(document).ready(function () {
     //Array of animals
     var topics = ['dog', 'cat', 'rabbit', 'hamster', 'pig', 'cow', 'horse'];
 
-    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=GrhtSLiXUm1xgGnoDvs5AdIBFS6TJQLC&tag=cats";
-
     //Generate buttons using items from topics array and append to buttons div
-
     function generate() {
+        $("#buttons").empty();
         for (var i = 0; i < topics.length; i++) {
             var button = $("<button>");
-            button.attr("id", topics[i]);
+            button.attr("data-name", topics[i]);
+            button.addClass("btn btn-primary");
             button.text(topics[i]);
             $("#buttons").append(button);
         }
     };
 
     generate();
+
+    //Click handler for dynamically generated topics buttons
+    $(document.body).on("click", ".btn-primary", function () {
+        var animal = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=GrhtSLiXUm1xgGnoDvs5AdIBFS6TJQLC&limit=10";
+        
+        //Make AJAX request
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
+            //Retrieve gif URL and stores it in imageURL variable
+            var imageUrl = response.data.image_original_url;
+            
+        });
+    });
 
 });

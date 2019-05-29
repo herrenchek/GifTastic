7be1a63850997222b7d1d8e5c909ffbc22b47c05
Giselle Malenchek <Giselle@Giselles-MBP.fios-router.home>
2019-05-29 01:24:12 -0400

    Click handlers work

--- assets/app.js
+++ assets/app.js
@@ -17,26 +17,41 @@ $(document).ready(function () {
 
     generate();
 
+    $("#submit").on("click", function (event) {
+        event.preventDefault();
+        //Grab the input from the textbox
+        var another = $("#input").val().trim();
+        //Add new Pokemon to topics array
+        topics.push(another);
+        //Calling the function that handles the processing of the topics array
+        generate();
+    });
+
     //Click handler for dynamically generated topics buttons
     $(document.body).on("click", ".btn-primary", function () {
         var pokemon = $(this).attr("data-name");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + pokemon + "&api_key=GrhtSLiXUm1xgGnoDvs5AdIBFS6TJQLC&limit=10";
-        
+
         //Make AJAX request
         $.ajax({
             url: queryURL,
             method: "GET"
-        }).then(function(response) {
+        }).then(function (response) {
             console.log(response);
             //Retrieve gif URL and stores it in imageURL variable
             var imageURL = response.data[0].images.fixed_height.url;
+
+            // var rating = response.data[0].rating;
+            // // Creates an element to have the rating displayed
+            // var p = $("<p>").text("Rating: " + rating);
+            // $("#gifs").prepend(p);
+
             //Creating an image
             var image = $("<img>");
 
             //Setting src and alt attributes on the image
             image.attr("src", imageURL);
-            image.attr("alt", pokemon + " image");
-            console.log(image);
+            image.attr("alt", pokemon + " image.");
             //Add it to the page
             $("#gifs").prepend(image);
         });
